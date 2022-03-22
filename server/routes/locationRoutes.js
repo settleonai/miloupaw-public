793d49b726c184a30e1ec05113bfa67e3eb660f6
farrokh <saytec@gmail.com>
2022-03-21 17:55:22 -0700

    route update

--- server/routes/locationRoutes.js
+++ server/routes/locationRoutes.js
@@ -10,7 +10,7 @@ const { protect } = require("../middleware/authMiddleware");
 
 router.get("/", protect, getMyLocations);
 router.post("/", protect, addLocation);
-router.put("/", protect, updateLocation);
-router.delete("/", protect, deleteLocation);
+router.put("/:id", protect, updateLocation);
+router.delete("/:id", protect, deleteLocation);
 
 module.exports = router;
