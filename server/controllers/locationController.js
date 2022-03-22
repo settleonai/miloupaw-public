70d4773dc31c37618c36186a04a6d478e44f5c53
farrokh <saytec@gmail.com>
2022-03-21 17:44:39 -0700

    update and delete added

--- server/controllers/locationController.js
+++ server/controllers/locationController.js
@@ -10,7 +10,7 @@ const getMyLocations = asyncHandler(async (req, res) => {
   console.log("req user", req.user);
   try {
     // find users locations
-    const locations = await Location.find({ user: req.user.id });
+    const locations = await Location.find({ user: req.user._id });
     console.log("locations", locations);
 
     res.status(200).json(locations);
@@ -61,4 +61,82 @@ const addLocation = asyncHandler(async (req, res) => {
   res.status(201).json(profile);
 });
 
-module.exports = { getMyLocations, addLocation };
+// @desc    Update a location
+// @route   PUT /api/location/:id
+// @access  Private
+const updateLocation = asyncHandler(async (req, res) => {
+  const {
+    title,
+    address,
+    coordinates,
+    uses_keys,
+    has_alarm,
+    has_camera,
+    alarm_code,
+    access_parking_notes,
+    additional_notes,
+    photos,
+  } = req.body;
+
+  // find location
+  const location = await Location.findOne({ _id: req.params.id });
+
+  // check if user owns location or is admin
+  if (req.user._id !== location.user.toString() && req.user.role !== "admin") {
+    return res.status(401).json({ msg: "Not authorized" });
+  }
+
+  // update location fields
+  location.title = title;
+  location.address = address;
+  location.coordinates = coordinates;
+  location.uses_keys = uses_keys;
+  location.has_alarm = has_alarm;
+  location.has_camera = has_camera;
+  location.alarm_code = alarm_code;
+  location.access_parking_notes = access_parking_notes;
+  location.additional_notes = additional_notes;
+  location.photos = photos;
+
+  // save location
+  await location.save();
+
+  // find profile
+  const profile = await Profile.findOne({ user: req.user._id })
+    .populate("locations")
+    .populate("pets");
+
+  // return profile
+  res.status(200).json(profile);
+});
+
+// @desc    Delete a location
+// @route   DELETE /api/location/:id
+// @access  Private
+const deleteLocation = asyncHandler(async (req, res) => {
+  // find location
+  const location = await Location.findOne({ _id: req.params.id });
+
+  // check if user owns location or is admin
+  if (req.user._id !== location.user.toString() && req.user.role !== "admin") {
+    return res.status(401).json({ msg: "Not authorized" });
+  }
+
+  // remove location
+  await location.remove();
+
+  // find profile
+  const profile = await Profile.findOne({ user: req.user._id })
+    .populate("locations")
+    .populate("pets");
+
+  // return profile
+  res.status(200).json(profile);
+});
+
+module.exports = {
+  getMyLocations,
+  addLocation,
+  updateLocation,
+  deleteLocation,
+};
--- server/routes/locationRoutes.js
+++ server/routes/locationRoutes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const {
   getMyLocations,
   addLocation,
+  updateLocation,
 } = require("../controllers/locationController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.get("/", protect, getMyLocations);
 router.post("/", protect, addLocation);
+router.put("/", protect, updateLocation);
+router.delete("/", protect, deleteLocation);
 
 module.exports = router;
