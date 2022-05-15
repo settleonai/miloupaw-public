bfefac8a49f680bbf492f94e2e71c111ad602b19
farrokh <saytec@gmail.com>
2022-05-15 16:34:39 -0700

    bug fix

--- web/ecosystem.config.js
+++ web/ecosystem.config.js
@@ -2,9 +2,10 @@ module.exports = {
   apps: [
     {
       name: "miloupawWebServer",
-      script: "./server/server.js",
       watch: true,
-      script: "yarn start",
+      cwd: "./",
+      script: "yarn",
+      args: "start",
       env: {
         PORT: 3000,
         NODE_ENV: "development",
