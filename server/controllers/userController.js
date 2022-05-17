4fb8ca808f4c4ecbb4f0e2f96e89309cf26a4535
farrokh <saytec@gmail.com>
2022-05-16 22:55:11 -0700

    et

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -141,8 +141,8 @@ const updateMyProfile = asyncHandler(async (req, res) => {
     ) {
       // push new picture to pictures array
       userObj.pictures.unshift(req.body.picture);
-    } else if (req.body.picture) {
-      userObj.pictures.unshift(req.body.picture);
+    } else if (req.body.picture & !userObj.pictures) {
+      userObj.pictures = req.body.picture;
     }
     if (req.body.first_name || req.body.last_name) {
       userObj.name = req.body.first_name + " " + req.body.last_name;
