79a2872509ea92822cf89aa20b7bdba5cc301565
farrokh <saytec@gmail.com>
2022-05-24 18:03:26 -0700

    pet update

--- server/controllers/petController.js
+++ server/controllers/petController.js
@@ -2,6 +2,12 @@ const asyncHandler = require("express-async-handler");
 const User = require("../../models/userModel");
 const Pet = require("../../models/petModel");
 const Profile = require("../../models/profileModel");
+const {
+  DOG_BREEDS,
+  CAT_BREEDS,
+  DOG_BREEDS_KEYS,
+  CAT_BREEDS_KEYS,
+} = require("../utils/pet-breeds");
 
 // @desc    Get user pets
 // @route   GET /api/pets/
@@ -115,4 +121,57 @@ const getPet = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { getMyPets, addPet, updatePet, getPet };
+// @desc    get breed
+// @route   GET /api/pets/pet-breeds/:breed
+// @access  Private
+const getBreed = asyncHandler(async (req, res) => {
+  console.log("getBreed");
+  try {
+    const breed = req.params.breed;
+    const type = req.params.type;
+    let list;
+
+    if (!type) {
+      return res.status(400).json({
+        success: false,
+        error: "Please provide a type",
+      });
+    }
+
+    if (type === "dog") {
+      list = DOG_BREEDS_KEYS;
+    }
+    if (type === "cat") {
+      list = CAT_BREEDS_KEYS;
+    }
+
+    const regexp = new RegExp(breed, "i");
+    const results = list
+      .filter((breed) => regexp.test(breed))
+      .slice(0, 4)
+      .map((breed) => {
+        const result = type === "dog" ? DOG_BREEDS[breed] : CAT_BREEDS[breed];
+        return {
+          breed: breed,
+          ...result,
+        };
+      });
+
+    if (!results) {
+      return res.status(400).json({
+        success: false,
+        error: "No breed found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      result: results,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+module.exports = { getMyPets, addPet, updatePet, getPet, getBreed };
--- server/routes/petRoutes.js
+++ server/routes/petRoutes.js
@@ -5,11 +5,13 @@ const {
   addPet,
   updatePet,
   getPet,
+  getBreed,
 } = require("../controllers/petController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.get("/", protect, getMyPets);
 router.post("/", protect, addPet);
+router.get("/pet-breeds/:breed/:type", protect, getBreed);
 router.get("/:id", protect, getPet);
 router.put("/:id", protect, updatePet);
 // router.delete("/:id", protect, deleteLocation);
