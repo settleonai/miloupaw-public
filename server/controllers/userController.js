1108d4fe376a84f05ad57ad5453c257327acb790
farrokh <saytec@gmail.com>
2022-06-03 15:44:45 -0700

    test

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -157,38 +157,45 @@ const updateMyProfile = asyncHandler(async (req, res) => {
     //   .model("User")
     //   .schema.add({ pictures: [String] });
 
-    console.log("userModel.schema.indexes()", User.schema.obj);
+    // console.log("userModel.schema.indexes()", User.schema.obj);
 
     const userObj = await User.findById(req.user.id);
 
-    console.log("userObj", userObj);
-    console.log("userObj.pictures", userObj.pictures);
+    // console.log("userObj", userObj);
+    // console.log("userObj.pictures", userObj.pictures);
 
-    console.log("userObj.pictures?.length > 0", userObj.pictures?.length > 0);
+    // console.log("userObj.pictures?.length > 0", userObj.pictures?.length > 0);
     // console.log(
     //   "req.body.picture !== userObj.pictures[0]",
     //   req.body.picture !== userObj.pictures[0]
     // );
 
-    if (
-      req.body.picture &&
-      userObj.pictures?.length > 0 &&
-      req.body.picture !== userObj.pictures[0]
-    ) {
-      // push new picture to pictures array
-      userObj.pictures.unshift(req.body.picture);
-      console.log("req.body.picture", req.body.picture);
-      console.log("profile", userObj.pictures);
-    } else if (req.body.picture && userObj.pictures.length === 0) {
-      console.log("before add", [req.body.picture]);
-      userObj.pictures = [req.body.picture];
+    if (req.body.picture) {
+      User.updateOne(
+        { _id: req.user.id },
+        { $addToSet: { pictures: req.body.picture } }
+      );
     }
+
+    // if (
+    //   req.body.picture &&
+    //   userObj.pictures?.length > 0 &&
+    //   req.body.picture !== userObj.pictures[0]
+    // ) {
+    //   // push new picture to pictures array
+    //   userObj.pictures.unshift(req.body.picture);
+    //   console.log("req.body.picture", req.body.picture);
+    //   console.log("profile", userObj.pictures);
+    // } else if (req.body.picture && userObj.pictures.length === 0) {
+    //   console.log("before add", [req.body.picture]);
+    //   userObj.pictures = [req.body.picture];
+    // }
     if (req.body.first_name || req.body.last_name) {
       userObj.name = req.body.first_name + " " + req.body.last_name;
     }
     await userObj.save();
-    console.log("----saved userObj", userObj);
-    console.log("----saved userObj.pictures:", userObj.pictures);
+    // console.log("----saved userObj", userObj);
+    // console.log("----saved userObj.pictures:", userObj.pictures);
 
     res.status(200).json({
       success: true,
