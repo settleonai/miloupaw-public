faf0b1c24e9dac8462affe9285d08ce939901cb0
farrokh <saytec@gmail.com>
2022-06-03 11:22:37 -0700

    test

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -154,7 +154,8 @@ const updateMyProfile = asyncHandler(async (req, res) => {
     }
     const userObj = await User.findById(req.user.id);
 
-    console.log("userObj", userObj.pictures);
+    console.log("userObj", userObj);
+    console.log("userObj.pictures", userObj.pictures);
 
     console.log("userObj.pictures?.length > 0", userObj.pictures?.length > 0);
     console.log(
