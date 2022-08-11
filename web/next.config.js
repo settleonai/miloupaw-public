1ab6f7d8757274323c9017cd06354acbbb523031
farrokh <saytec@gmail.com>
2022-08-10 17:26:56 -0700

    android play link

--- web/next.config.js
+++ web/next.config.js
@@ -2,11 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com", "res.cloudinary.com"],
+    domains: ["images.unsplash.com", "res.cloudinary.com","play.google.com"],
   },
   env: {
     APP_STORE_ID: "1620724992",
     APP_STORE_LINK: "https://apps.apple.com/us/app/miloupaw/id1620724992",
+    GOOGLE_STORE_LINK:
+      "https://play.google.com/store/apps/details?id=com.fnel.miloupaw",
   },
 };
 
